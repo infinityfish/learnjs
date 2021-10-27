b07066986005d829b7e7ba658fe6c18bd482b811
infinityfish <ericmabo@gmail.com>
2021-10-27 12:00:00 -0400

    added nextjs basic setup

--- Readme.md
+++ Readme.md
@@ -1,3 +1,9 @@
 
 This is a basic setup to slowly add React to a website
 
+NextJS app dev
+
+1. created new nextjsapp
+2. yarn add react react-dom next
+3. create pages/index.jsx 
+4. craeted and added scrpits to package.json
--- nextjsapp/pages/index.jsx
+++ nextjsapp/pages/index.jsx
@@ -1,23 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="styles.css">
-    <title>Basic React Component</title>
-</head>
-<body>
-    <div id="app"></div>
-    
-</body>
-<script crossorigin src="https://unpkg.com/react@17/umd/react.production.min.js"></script>
-<script crossorigin src="https://unpkg.com/react-dom@17/umd/react-dom.production.min.js"></script>
-<script src="https://unpkg.com/babel-standalone@6/babel.min.js"></script>
-
-<script type="text/jsx">
-
-    const app = document.getElementById("app");
+import React from 'react'
 
     function Header({ title }){
         return (
@@ -36,7 +17,7 @@
         )
     }
 
-    function HomePage(){
+   export default function HomePage(){
 
         const students = ['ola', 'mary', 'ife']
         
@@ -60,8 +41,3 @@
             </div>
         )    
     }
-
-    ReactDOM.render(<HomePage />, app);
-
-</script>
-</html>
